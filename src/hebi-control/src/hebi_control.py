f45948ab6b318d620556ae0b041b316ec286ea3a
Michael Lin <michaelv03@gmail.com>
2021-01-24 12:15:55 -0800

    Adding a sbuscriber for XBox controller

--- src/hebi-control/src/hebi_control.py
+++ src/hebi-control/src/hebi_control.py
@@ -8,9 +8,10 @@
 
 import numpy as np
 import math
-import hebi
+#import hebi
 from time import sleep
 from std_msgs.msg import Float64
+from sensor_msgs.msg import Joy
 import rospy
 
 class HebiRobot(object):
@@ -30,37 +31,41 @@ class HebiRobot(object):
 
   """
 
-  def __init__(self, family='X8-9', names=['X-80749']):
-    self.position = 0.0   # variable for sensor position reading
+  def __init__(self, family='X8-9', names=['X-80749'], noHw = False):
+    self.joint_position = 0.0   # variable for sensor position reading
     self.vel_des = 0.0    # variable for velocity control
 
     # store hebi actuator params
     self.family = family
     self.actuator_names = names
 
-    # automatic lookup of actuators connected to the network
-    lookup = hebi.Lookup()
-    # Give the Lookup process 2 seconds to discover modules
-    sleep(2)
-    print('Modules found on network:')
-    for entry in lookup.entrylist:
-      print("{0} | {1}".format(entry.family, entry.name))
+    self.noHw = noHw
 
-    # get control group by family name and module name
-    self.group = lookup.get_group_from_names([self.family], self.actuator_names)
+    if (self.noHw):
+      # automatic lookup of actuators connected to the network
+      lookup = hebi.Lookup()
+      # Give the Lookup process 2 seconds to discover modules
+      sleep(2)
+      print('Modules found on network:')
+      for entry in lookup.entrylist:
+        print("{0} | {1}".format(entry.family, entry.name))
 
-    # create a group command object used to send commands to actuators
-    self.group_command = hebi.GroupCommand(self.group.size)
+      # get control group by family name and module name
+      self.group = lookup.get_group_from_names([self.family], self.actuator_names)
 
-    # set up the feedback callback function
-    self.group.add_feedback_handler(self.get_actuator_feedback)
+      # create a group command object used to send commands to actuators
+      self.group_command = hebi.GroupCommand(self.group.size)
+
+      # set up the feedback callback function
+      self.group.add_feedback_handler(self.get_actuator_feedback)
 
     # start ROS node
     rospy.init_node('hebi_control', disable_signals=True)
 
     # ROS publishers and subscribers
-    self.feedback_pub = rospy.Publisher('motor_pos', Float64, queue_size=3)
     self.vel_des_sub = rospy.Subscriber('vel_des', Float64, self.get_vel_des)
+    self.joy_sub = rospy.Subscriber('joy', Joy, self.get_joy_cmd)
+    self.feedback_pub = rospy.Publisher('motor_pos', Float64, queue_size=3)
 
     # set ROS loop rate to 100. Same as default feedback rate from HEBI.
     self.rate = rospy.Rate(100)
@@ -70,11 +75,15 @@ class HebiRobot(object):
 
     while True:
       # send feedback values
-      self.feedback_pub.publish(self.position)
+      self.feedback_pub.publish(self.joint_position)
 
-      # command desired velocities
-      self.group_command.velocity = self.vel_des
-      self.group.send_command(self.group_command)
+      # can do something with self.controllerCmds
+
+      if (self.noHw):
+        # command desired velocities
+        self.group_command.velocity = self.vel_des
+        self.group.send_command(self.group_command)
+
       self.rate.sleep()
 
   def get_vel_des(self, data):
@@ -95,17 +104,32 @@ class HebiRobot(object):
     """ Fetches HEBI actuator sensor data as a callback
 
     Args:
-      feedback: hebi.GroupFeedback instance contains position and velocity
+      feedback: hebi.GroupFeedback instance contains joint position and velocity
 
     Returns:
       Nothing
     """
-    self.position = feedback.position
+    self.joint_position = feedback.position
     return
+
+  def get_joy_cmd(self, data):
+    """ Fetches data from xbox 360 controller
+
+    Args:
+      data: of type Joy (Joy.buttons and Joy.axes)
+
+    Returns:
+      Nothing
+    """
+    # storing the left and right joystick axes values
+    print("hi")
+    self.controllerCmds = data.axes[0:3]
+    return
+    
 
 
 if __name__ == '__main__':
-  hebi = HebiRobot()
+  hebi = HebiRobot(noHw=True)
   try:
     hebi.run_loop()
   except KeyboardInterrupt:
